1ccabcd74bc5bc51e3f1e9cf0e1f4a402eead0e6
Too Seng Wei <sengweitoo@gmail.com>
2019-06-11 16:13:01 +0800

    update email and show warning when usingdesktop

--- src/index.js
+++ src/index.js
@@ -7,24 +7,34 @@ import ScoreBoard from "./scenes/scoreBoard.js"
 import Reward from "./scenes/reward.js"
 import Test from "./scenes/test.js"
 
-const game = new Phaser.Game({
-  parent: "phaser-game",
-  type: Phaser.AUTO,
-  width: '100%',
-  height: '100%',
-  mode: Phaser.Scale.NONE,
-  autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
-  dom: {
-    createContainer: true
-  },
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 300 }, 
-      debug: false
-    }
-  },
+let game;
 
-  scene: [Welcome, Register, PlayInstruction, Game, ScoreBoard, Reward],
-  // scene: [Reward],
-})
+let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+let element = document.getElementById('warning');
+if (isMobile) {
+    game = new Phaser.Game({
+    parent: "phaser-game",
+    type: Phaser.AUTO,
+    width: '100%',
+    height: '100%',
+    mode: Phaser.Scale.NONE,
+    autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+    dom: {
+      createContainer: true
+    },
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 300 }, 
+        debug: false
+      }
+    },
+
+    scene: [Welcome, Register, PlayInstruction, Game, ScoreBoard, Reward],
+    // scene: [Reward],
+  })
+
+} else {
+  element.innerHTML = "Please Use Mobile to play";
+}
+  
